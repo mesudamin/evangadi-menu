d3ecb444d7b3ddb242529dc8ec81a76f0411e8db
mesudamin <lalemasud@gmail.com>
2026-06-25 17:26:27 +0300

    fix  price wrting  system

--- src/App.jsx
+++ src/App.jsx
@@ -20,17 +20,17 @@ function App() {
   {
     id: 3,
     title: "GENFO (ገንፎ)",
-    price:" $20.99",
+    price:"$20.99",
     img: "https://cdn.tasteatlas.com//images/dishes/d64fe1f3c3754340bfbc7e20510110ef.jpg?w=905&h=510",
     desc: `Genfo is a simple Ethiopian porridge that is commonly consumed for breakfast, made by adding dry-roasted barley flour to boiling water and stirring the concoction with a wooden utensil until it develops a smooth, yet extremely thick consistency.`,
   },
   {
     id: 4,
     title: "KINCHE (ቂንጬ)",
-    price:" $7.99",
+    price:"$7.99",
     img: "https://cdn.tasteatlas.com//images/dishes/5746f1c174664a01990eb04377c9228a.jpg?w=905&h=510",
     desc: `Simple, nutritious, and inexpensive, kinche is an Ethiopian breakfast staple made with cracked wheat that is boiled in either water or milk. The best way to describe kinche would be as the Ethiopian equivalent of oatmeal. After it has been cooked, kinche is added to the pan with either clarified spiced butter ...`,
-    link: "Learn more"
+    
   },
 
   {
@@ -52,7 +52,7 @@ function App() {
   {
     id: 7,
     title: "GORED GORED (ጎረድ ጎረድ)",
-    price:" $28.99",
+    price:"$28.99",
     img: "https://www.willflyforfood.net/wp-content/uploads/2021/09/ethiopian-food-kitfo-gored.jpg.webp",
     desc: `If kitfo piqued your interest, then you may want to try gored gored as well. It’s a similar dish to kitfo except it’s made with cubes of raw beef – usually tenderloin or round steak – seasoned with niter kibbeh, mitmita, berbere, and awaze (chili mustard sauce). Like kitfo, it’s usually served with injera.`,
   },
@@ -60,7 +60,7 @@ function App() {
   {
     id: 8,
     title: "FATIRA (ፈጢራ)",
-    price:" $18.99",
+    price:"$18.99",
     img: "https://cdn.tasteatlas.com//images/dishes/f99dcc3dfcc642348a40b19f51f32b74.jpg?w=905&h=510",
     desc: `Fatira is a traditional Ethiopian street food item that is commonly consumed for breakfast, consisting of a large, crispy, wheat flour pancake. It is traditionally served with scrambled eggs, honey, or both. Fatira is often cut into smaller pieces, and it is especially popular during Eid-al-Fitr in Ethiopia.`,
   },
@@ -68,7 +68,7 @@ function App() {
   {
     id: 9,
     title: "KIXA FIR-FIR (ቂጣ ፍር ፍር)",
-    price:" $15.99",
+    price:"$15.99",
     img: "https://cdn.tasteatlas.com//images/dishes/685b5e5b70b441a7a4e7279a43a89333.jpg?w=905&h=510",
     desc: `Kixa fir-fir is a fit-fit variety prepared with a combination of torn pieces of kitcha flatbread, clarified butter, and berbere spices. The dish is traditionally served for breakfast, when it’s accompanied by plain yogurt. Unlike most Ethiopian dishes, kitcha fit-fit is typically consumed with a spoon instead of using the right hand. `,
   },
