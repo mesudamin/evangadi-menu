e33710f7b5d4c827f89b6293ebad522ccd43d831
mesudamin <lalemasud@gmail.com>
2026-06-25 11:57:27 +0300

    removing Literal backtick  from desc

--- src/Components/MenuF/MenuF.jsx
+++ src/Components/MenuF/MenuF.jsx
@@ -9,7 +9,7 @@ export default class MenuF extends Component {
     title= "TIMATIM SELAXA (ቲማቲም ሰላጣ)"
     price=" $5.99"
     img= "https://www.willflyforfood.net/wp-content/uploads/2021/09/ethiopian-food-timatim-salata.jpg.webp"
-    desc=" `Timatim Salata refers to a type of fresh Ethiopian tomato salad that’s also popular in Eritrea. It’s made with diced tomatoes, minced onions, and finely chopped peppers dressed with a mixture of berbere spices, olive oil, vinegar, and lemon juice.`"
+    desc=" Timatim Salata refers to a type of fresh Ethiopian tomato salad that’s also popular in Eritrea. It’s made with diced tomatoes, minced onions, and finely chopped peppers dressed with a mixture of berbere spices, olive oil, vinegar, and lemon juice."
 
         />
       </>
