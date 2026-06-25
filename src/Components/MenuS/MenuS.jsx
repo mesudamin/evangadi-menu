e367fd74ce5af26bb4b3ad190fb97fe8a78ab750
mesudamin <lalemasud@gmail.com>
2026-06-25 11:29:44 +0300

    remove long text from second com

--- src/Components/MenuS/MenuS.jsx
+++ src/Components/MenuS/MenuS.jsx
@@ -12,7 +12,7 @@ export default class MenuS extends Component {
     category= "lunch"
     price=" $22.99"
     img= "https://media.cnn.com/api/v1/images/stellar/prod/190205144959-shekla-tibs.jpg?q=w_1600,h_900,x_0,y_0,c_fill/w_1280"
-    desc=" `Sliced beef or lamb, pan-fried in butter, garlic and onion, tibs is one of the most popular dishes among Ethiopians. It comes in a variety of forms, varying in type, size or shape of the cuts of meat used, and can range from hot to mild or contain little to no vegetables. A particularly recommended variation is shekla tibs, in which the strips of meat arrive at your table roasting atop a clay pot stoked with hot coals – dramatic and delicious.`"
+    desc=" `Sliced beef or lamb, pan-fried in butter, garlic and onion, tibs is one of the most popular dishes among Ethiopians. It comes in a variety of forms, varying in type, size or shape of the cuts of meat used, and can range from hot to mild or contain ...`"
   
         
         
