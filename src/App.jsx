048e0024e22614417194042897586cd9e641c8f9
mesudamin <lalemasud@gmail.com>
2026-06-25 11:05:44 +0300

    create thired   menu component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import  "./commonResource/style.css"
 import Header from "./Components/Header/Header.jsx"
 import MenuF from "./Components/MenuF/MenuF.jsx"
 import MenuS from "./Components/MenuS/MenuS.jsx"
+import MenuTH from "./Components/MenuTH/MenuTH.jsx"
 function App() {
   
 
   return (
     <>
    <Header/>
-   <MenuF/> <MenuS/> 
-
+   <MenuF/> 
+   <MenuS/> 
+   <MenuTH/>
    
     </>
   )
---
+++ src/Components/MenuTH/MenuTH.jsx
@@ -0,0 +1,21 @@
+import { Component } from 'react'
+import MenuBluePrint from '../MenuBluePrint'
+
+export default class MenuTH extends Component {
+  render() {
+    return (
+      <>
+       < MenuBluePrint
+       
+    title= "GENFO (ገንፎ)"
+    category= "breakfast"
+    price= "$20.99"
+    img= "https://cdn.tasteatlas.com//images/dishes/d64fe1f3c3754340bfbc7e20510110ef.jpg?w=905&h=510"
+    desc=" `Genfo is a simple Ethiopian porridge that is commonly consumed for breakfast, made by adding dry-roasted barley flour to boiling water and stirring the concoction with a wooden utensil until it develops a smooth, yet extremely thick consistency.`"
+  
+       
+       />
+      </>
+    )
+  }
+}
