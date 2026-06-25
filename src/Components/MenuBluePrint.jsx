ec7e20c9b911e95a0c8b517b69c47477f9b34df5
mesudamin <lalemasud@gmail.com>
2026-06-25 07:34:17 +0300

    write menu bluePrint

--- src/Components/MenuBluePrint.jsx
+++ src/Components/MenuBluePrint.jsx
@@ -2,10 +2,30 @@ import  { Component } from 'react'
 
 export default class MenuBluePrint extends Component {
   render() {
+    const { img,title,  price,desc } = this.props;
     return (
-      <div>
-        
-      </div>
+      <>
+      
+         <div class="single-food">
+          <div class="img">
+            <img
+              src={img}
+            />
+          </div>
+          <div class="title-price">
+            <h3>{title}</h3>
+            <p>{price}</p>
+          </div>
+          <div class="food-desc">{desc}
+          </div>
+        </div>
+
+
+
+
+
+      
+      </>
     )
   }
 }
---
+++ src/Components/MenuF/MenuF.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+
+export default class MenuF extends Component {
+  render() {
+    return (
+      <>
+        
+      </>
+    )
+  }
+}
